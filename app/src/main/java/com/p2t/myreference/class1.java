a58a30f48c3880b56b911676bfecebacb34ad615
Arkar Myo <koarkarmyo@gmail.com>
2016-06-08 00:09:10 +0800

    started

--- app/src/main/java/com/p2t/myreference/class1.java
+++
@@ -1,7 +0,0 @@
-package com.p2t.myreference;
-
-/**
- * Created by arkarmyo on 7/6/16.
- */
-public class class1 {
-}
--- app/src/main/java/com/p2t/myreference/class3.java
+++
@@ -1,7 +0,0 @@
-package com.p2t.myreference;
-
-/**
- * Created by Arkar on 6/7/2016.
- */
-public class class3 {
-}
